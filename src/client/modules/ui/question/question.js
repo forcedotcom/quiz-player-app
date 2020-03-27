831ef05f616965b838c5ddeb8e62d0733d6c38c2
Philippe Ozil <5071767+pozil@users.noreply.github.com>
2020-03-27 14:36:42 +0100

    Added question label (#15)
    * Pushing question label to player app
    * Applied style to question label
    * Bumped dependencies and version

--- src/client/modules/ui/question/question.js
+++ src/client/modules/ui/question/question.js
@@ -1,6 +1,7 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class Question extends LightningElement {
+    @api label;
     isSaving;
 
     connectedCallback() {
