0609ff9ee40372eb3e2e062a25990a56562fb27a
pozil <pozil@salesforce.com>
2022-10-02 11:34:05 +0200

    feat: merged message and spinner

--- src/client/modules/ui/spinner/spinner.js
+++ src/client/modules/ui/spinner/spinner.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class Spinner extends LightningElement {}
+export default class Spinner extends LightningElement {
+    @api message;
+}
